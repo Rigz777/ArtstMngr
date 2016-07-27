3943114a3cadd9e1e08405254ac51eb2482de7ed
cornerstonian <vois.cornerstone@gmail.com>
2016-07-26 21:48:15 -0500

    added user authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+      account = Account.find_by email: params[:email]
+      if account && account.authenticate(params[:password])
+        session[:account_id] = account.id
+        redirect_to root_path, notice: "You are signed in as #{account.email}"
+      else
+        flash.now[:alert] = "Something is wrong w/ your email and/or password"
+        render :new
+      end
+  end
+
+  def delete
+    session.delete :account_id
+    redirect_to root_path, notice: "You Are Now Signed Out"
+  end
+
+end
