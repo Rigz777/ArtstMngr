17badd5077f176d0d1be65fc8de3296b64a12125
cornerstonian <vois.cornerstone@gmail.com>
2016-08-08 20:52:03 -0500

    travis again

--- test/features/login_can_view_dashboard_test.rb
+++ test/features/login_can_view_dashboard_test.rb
@@ -18,7 +18,7 @@ class LoginCanViewDashboardTest < Capybara::Rails::TestCase
     fill_in('Email', with: 'vois@recordlabel.com')
     fill_in('Password', with: '12345678')
     click_button('Sign In')
-    assert_content page, "Dashboard"
+    assert_content page, "ArtstMngr"
     refute_content page, "Sign In"
   end
 
